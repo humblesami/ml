208ca04c5a5ecec171b965db1b57c321dd2de43c
humblesami <samiakram@live.com>
2024-03-26 18:15:40 +0500

    algos

--- algos.py
+++ algos.py
@@ -10,25 +10,45 @@ import tensorflow.compat.v2.feature_column as fc
 
 import tensorflow as tf
 
-x = [1, 2, 2.5, 3, 4]
-y = [1, 4, 7, 9, 15]
-plt.plot(x, y, 'ro')
-plt.axis([0, 6, 0, 20])
 
-plt.plot(x, y, 'ro')
-plt.axis([0, 6, 0, 20])
-plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)))
+def plot_linear():
+    x = [1, 2, 2.5, 3, 4]
+    y = [1, 4, 7, 9, 15]
+    plt.plot(x, y, 'ro')
+    plt.axis([0, 6, 0, 20])
 
-# plt.show()
+    plt.plot(x, y, 'ro')
+    plt.axis([0, 6, 0, 20])
+    plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)))
+
 
 dftrain = pd.read_csv('./datasets/titanic/train.csv') # training data
 dfeval = pd.read_csv('./datasets/titanic/eval.csv') # testing data
 y_train = dftrain.pop('survived')
 y_eval = dfeval.pop('survived')
 
-dftrain.head()
-dftrain.describe()
+print(dftrain.head())
+print(dftrain.describe())
+print(dftrain.shape)
+print(y_train.head())
 
-dftrain.shape
-y_train.head()
-print('\n\nLove')
+
+def plot_age():
+    dftrain.age.hist(bins=20)
+
+
+def plot_sex():
+    dftrain.sex.value_counts().plot(kind='barh')
+
+
+def plot_counts():
+    dftrain['class'].value_counts().plot(kind='barh')
+
+
+def plot_survived():
+    pd.concat([dftrain, y_train], axis=1).groupby('sex').survived.mean().plot(kind='barh').set_xlabel('% survive')
+
+
+plot_survived()
+plt.show()
+
